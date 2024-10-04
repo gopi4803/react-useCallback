66e2bd345edd90009feb946decfa7fcc970f9175
Gopichand Bhimavarapu <gopichand.bhimavarapu@ibm.com>
2024-10-04 17:36:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       {/* <ParentComponent /> */}
       {/* <FocusInput /> */}
       {/* <DocTitleOne /> */}
-      <DocTitleTwo />
-      <Counter />
+      {/* <DocTitleTwo />
+      <Counter /> */}
       <CounterOne />
       <CounterTwo />
       {/* <ParentComponent />
